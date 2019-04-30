5995de79479ee3f6beab3db3ce9d4a03a171df17
Suprianto D <ersaka96@gmail.com>
2019-04-30 22:18:42 +0800

    u14
    u14

--- src/com/erthru/pthajratabadiservismobil/utils/ApiEndPoint.java
+++ src/com/erthru/pthajratabadiservismobil/utils/ApiEndPoint.java
@@ -11,24 +11,24 @@ package com.erthru.pthajratabadiservismobil.utils;
  */
 public class ApiEndPoint {
     
-    public static String DAFTAR_BARANG_SERVIS = "http://localhost/anows/api_pthabgsm/app.php?mod=daftar_barang_servis";
-    public static String SEARCH_BARANG_SERVIS = "http://localhost/anows/api_pthabgsm/app.php?mod=search_barang_servis";
-    public static String ADD_BARANG_SERVIS = "http://localhost/anows/api_pthabgsm/app.php?mod=add_barang_servis";
-    public static String UPDATE_BARANG_SERVIS = "http://localhost/anows/api_pthabgsm/app.php?mod=update_barang_servis";
-    public static String DELETE_BARANG_SERVIS = "http://localhost/anows/api_pthabgsm/app.php?mod=delete_barang_servis";
-    public static String USER_ALL = "http://localhost/anows/api_pthabgsm/app.php?mod=user_all";
-    public static String USER_SEARCH = "http://localhost/anows/api_pthabgsm/app.php?mod=user_search";
-    public static String DAFTAR_BOOKING_ALL = "http://localhost/anows/api_pthabgsm/app.php?mod=daftar_booking_all";
-    public static String SEARCH_BOOKING_ALL = "http://localhost/anows/api_pthabgsm/app.php?mod=search_booking_all";
-    public static String DAFTAR_BOOKING_ALL_DATE_FILTER = "http://localhost/anows/api_pthabgsm/app.php?mod=daftar_booking_all_date_filter";
-    public static String DAFTAR_BOOKING_DETAIL = "http://localhost/anows/api_pthabgsm/app.php?mod=daftar_booking_detail";
-    public static String TERIMA_BOOKING = "http://localhost/anows/api_pthabgsm/app.php?mod=terima_booking";
-    public static String DAFTAR_BOOKING_STATUS = "http://localhost/anows/api_pthabgsm/app.php?mod=daftar_booking_status";
-    public static String PEMILIHAN_PART_BOOKING = "http://localhost/anows/api_pthabgsm/app.php?mod=pemilihan_part_booking";
-    public static String DAFTAR_BOOKING_ITEM = "http://localhost/anows/api_pthabgsm/app.php?mod=daftar_booking_item";
-    public static String DALAM_PENGERJAAM_BOOKING = "http://localhost/anows/api_pthabgsm/app.php?mod=dalam_pengerjaan_booking";
-    public static String SELESAI_BOOKING = "http://localhost/anows/api_pthabgsm/app.php?mod=selesai_booking";
-    public static String DITOLAK_BOOKING = "http://localhost/anows/api_pthabgsm/app.php?mod=ditolak_booking";
-    public static String LOGIN_ADMIN = "http://localhost/anows/api_pthabgsm/app.php?mod=login_admin";
+    public static String DAFTAR_BARANG_SERVIS = "http://ertohru.com/pthabgsm/App.php?mod=daftar_barang_servis";
+    public static String SEARCH_BARANG_SERVIS = "http://ertohru.com/pthabgsm/App.php?mod=search_barang_servis";
+    public static String ADD_BARANG_SERVIS = "http://ertohru.com/pthabgsm/App.php?mod=add_barang_servis";
+    public static String UPDATE_BARANG_SERVIS = "http://ertohru.com/pthabgsm/App.php?mod=update_barang_servis";
+    public static String DELETE_BARANG_SERVIS = "http://ertohru.com/pthabgsm/App.php?mod=delete_barang_servis";
+    public static String USER_ALL = "http://ertohru.com/pthabgsm/App.php?mod=user_all";
+    public static String USER_SEARCH = "http://ertohru.com/pthabgsm/App.php?mod=user_search";
+    public static String DAFTAR_BOOKING_ALL = "http://ertohru.com/pthabgsm/App.php?mod=daftar_booking_all";
+    public static String SEARCH_BOOKING_ALL = "http://ertohru.com/pthabgsm/App.php?mod=search_booking_all";
+    public static String DAFTAR_BOOKING_ALL_DATE_FILTER = "http://ertohru.com/pthabgsm/App.php?mod=daftar_booking_all_date_filter";
+    public static String DAFTAR_BOOKING_DETAIL = "http://ertohru.com/pthabgsm/App.php?mod=daftar_booking_detail";
+    public static String TERIMA_BOOKING = "http://ertohru.com/pthabgsm/App.php?mod=terima_booking";
+    public static String DAFTAR_BOOKING_STATUS = "http://ertohru.com/pthabgsm/App.php?mod=daftar_booking_status";
+    public static String PEMILIHAN_PART_BOOKING = "http://ertohru.com/pthabgsm/App.php?mod=pemilihan_part_booking";
+    public static String DAFTAR_BOOKING_ITEM = "http://ertohru.com/pthabgsm/App.php?mod=daftar_booking_item";
+    public static String DALAM_PENGERJAAM_BOOKING = "http://ertohru.com/pthabgsm/App.php?mod=dalam_pengerjaan_booking";
+    public static String SELESAI_BOOKING = "http://ertohru.com/pthabgsm/App.php?mod=selesai_booking";
+    public static String DITOLAK_BOOKING = "http://ertohru.com/pthabgsm/App.php?mod=ditolak_booking";
+    public static String LOGIN_ADMIN = "http://ertohru.com/pthabgsm/App.php?mod=login_admin";
     
 }
